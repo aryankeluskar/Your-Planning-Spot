b3fd43d2a6563af302dd2ac4067025cb3b9d67c6
Aryan K <aryan.keluskar@icloud.com>
2021-06-04 09:20:19 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express();
-var items = [];
+var itarr = [];
 
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/tdList", {useNewUrlParser: true, useUnifiedTopology: true})
+const tdlSchema = {
+    name: String
+};
+const items = mongoose.model("items", tdlSchema);
+
+const i1 = new items({
+    name: "Welcome to To Do list"
+});
+const i2 = new items({
+    name: "Type Something and Hit Add Button"
+});
+const i3 = new items({
+    name: "<- Check the box to delete an item"
+});
+
 app.get("/", function(req, res) {
     var options = {
         weekday: "long",
@@ -15,12 +33,23 @@ app.get("/", function(req, res) {
     var today = new Date();
     var day = today.toLocaleDateString("en-US", options)
 
-    res.render('list', { Day: day, newli: items });
+    items.find({}, function(err, found){
+        if(found.length==0){           
+            items.insertMany([i1, i2, i3], function(err) {
+                console.log(err);
+            });
+         }
+        // console.log(found[i].name);    
+        res.render('list', { Day: day, newli: found });
+    });
 });
 
 app.post("/", function(req, res) {
     item = req.body.newItem;
-    items.push(item)
+    const ni = new items({
+        name: item
+    });
+    ni.save();
     console.log(item);
     res.redirect("/")
 })

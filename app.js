b9e2e6768f0fc710451989de8b5cb60a64fae86d
Aryan K <aryan.keluskar@icloud.com>
2021-06-18 18:25:03 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -65,4 +65,4 @@ app.post("/delete", function(req, res) {
 
 });
 
-app.listen(3000, function() { console.log("server started at http://localhost:3000"); })
+app.listen(process.env.PORT || 3000, function() { console.log("server started at http://localhost:3000"); })

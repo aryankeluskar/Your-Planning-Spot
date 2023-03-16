0fe6990315e0d999da12d1005c1257d4fba8eea3
Aryan Keluskar <aryankeluskar6@icloud.com>
2023-03-16 23:55:00 +0530

    added name login

--- app.mjs
+++ app.mjs
@@ -3,45 +3,18 @@
 // code to handle MongoDB below
 // ------------------------------
 
-
 import express from 'express';
 import pkg from 'mongodb';
-const { MongoClient } = pkg;
+let { MongoClient } = pkg;
 import { } from 'dotenv/config' // module
 
 // Replace the following with your Atlas connection string
-const url = "mongodb+srv://" + process.env.USERID + ":" + process.env.PASS + "@" + process.env.DBNAME + "/?retryWrites=true&w=majority";
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+let url = "mongodb+srv://" + process.env.USERID + ":" + process.env.PASS + "@" + process.env.DBNAME + "/?retryWrites=true&w=majority";
+let client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
-        await client.connect();
-        console.log("Connected correctly to server");
-        const db = client.db("finaltrial");
-        // Use the collection "people"
-        const col = db.collection("usersdata");
-        // Construct a document                                                                                                                                                              
-        // let personDocument = {
-        //     "name": { "userfirst": "Alan", "userlast": "Turing" },
-        //     "theirlist": ["Turing machine", "Turing test", "Turingery"],
-        // }
-
-        const query = {"_id":{"$oid":"6410b433cbac6ff8e65a6dd2"},"name":{"userfirst":"Alan"},"theirlist":["Turing Machine"]};
-        const optionsf = {
-            // Include only the `name` and `theirList` fields in the returned document
-            projection: { _id: 0, name: 1, theirlist: 1 },
-          };
-      
-        const personDocument = col.findOne(query, optionsf);
-        console.log(personDocument);
-
-        // ------------------------------
-        // code to render the app with the list below
-        // ------------------------------
-
-
-        const app = express();
-
+        let app = express();
         app.set('view engine', 'ejs');
         app.use(express.static("public"));
         app.use(express.urlencoded({
@@ -49,57 +22,122 @@ async function run() {
         }));
         app.use(express.json());
 
-        var options = {
-            // for displaying the dat at the top of the todolist
+        let dayoptions = {
+            // for displaying the date at the top of the todolist
             weekday: "long",
             month: "long",
             day: "numeric"
         }
 
-        let actualList = personDocument.theirlist;
-
-        console.log(actualList);
-
-        app.get("/", function (req, res) {
-            // home page render    
-            var today = new Date();
-            var day = today.toLocaleDateString("en-US", options)
-            res.render('list', { Day: day, newli: actualList });
-        });
-
-        app.post("/", async function (req, res) {
-            // adding item
-            console.log("adding item of value: " + req.body.newItem);
-
-            // create a filter for a movie to update
-            const filter = { "name": { "userfirst": "Alan", "userlast": "Turing" } };
-            // this option instructs the method to create a document if no documents match the filter
-            const options = { upsert: true };
-
-            const updateDoc = {
-                $set: {
-                  theirlist: actualList.push(req.body.newItem)
-                },
-              };
-              const result = await col.updateOne(filter, updateDoc, options);
-              console.log(
-                `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
-              );
-
-            
-            res.redirect("/");
-        });
-
-        app.post("/delete", function (req, res) {
-            // removing item, using the item index to splice it from array
-            console.log("Removing item of index: " + Number(Object.keys(req.body)[0]));
-            actualList.splice(Number(Object.keys(req.body)[0]), 1)
-            res.redirect("/");
-        });
+        await client.connect();
+        console.log("Connected correctly to server");
+        let db = client.db("finaltrial");
+        // Use the collection "usersdata"
+        let col = db.collection("usersdata");
 
         app.listen(3000, function () {
             console.log("Server started at http://localhost:3000");
         });
+
+        app.get("/", function (req, res) {
+            res.render('signup');
+        });
+
+        app.post("/signed", async function (req, res) {
+            console.log("Adding user with name: " + req.body.namein);
+
+            // create a filter for a movie to update
+            let filter = { name: {userfirst: req.body.namein }};
+            console.log(filter);
+            // this option instructs the method to create a document if no documents match the filter
+            let options = { upsert: true };
+
+            col.findOne(filter, function (err, personDocument) {
+                if (personDocument) {
+                    let actualList = personDocument.theirlist;
+                    console.log(actualList);
+
+                    res.redirect("/app");
+
+                    app.get("/app", function (req, res) {
+                        // home page render    
+                        let today = new Date();
+                        let day = today.toLocaleDateString("en-US", dayoptions)
+                        res.render('list', { Day: day, newli: actualList, theirname: personDocument.name.userfirst });
+                    });
+
+                    app.get("/signup", function (req, res) {
+                        let today = new Date();
+                        let day = today.toLocaleDateString("en-US", options)
+                        res.render('signup');
+                    });
+
+
+                    app.post("/app", async function (req, res) {
+                        // adding item
+                        console.log("adding item of value: " + req.body.newItem);
+
+                        // create a filter for a movie to update
+                        let filter = { name: personDocument.name };
+
+                        // this option instructs the method to create a document if no documents match the filter
+                        let options = { upsert: true };
+
+                        // updating the list
+                        actualList.push(req.body.newItem)
+
+                        let updateDoc = {
+                            $set: {
+                                theirlist: actualList
+                            },
+                        };
+                        let result = await col.updateOne(filter, updateDoc, options);
+                        console.log(
+                            `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+                        );
+
+                        // return to homepage
+                        res.redirect("/app");
+                    });
+
+
+
+                    app.post("/delete", async function (req, res) {
+                        // removing item, using the item index to splice it from array
+                        console.log("Removing item of index: " + Number(Object.keys(req.body)[0]));
+                        actualList.splice(Number(Object.keys(req.body)[0]), 1)
+
+                        // create a filter for a movie to update
+                        let filter = { name: personDocument.name };
+
+                        // this option instructs the method to create a document if no documents match the filter
+                        let options = { upsert: true };
+
+                        let updateDoc = {
+                            $set: {
+                                theirlist: actualList
+                            },
+                        };
+                        let result = await col.updateOne(filter, updateDoc, options);
+                        console.log(
+                            `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+                        );
+
+                        res.redirect("/app");
+                    });
+
+                    
+
+                }
+                if (err) {
+                    console.log("error occurred");
+                    console.log(err);
+                    res.redirect('/signup')
+                }
+            });
+        });
+
+
 
     } catch (err) {
         console.log(err.stack);
